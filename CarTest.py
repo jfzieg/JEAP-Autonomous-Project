1d7190f3fcd1f8e99a0ffc134308101b1a5511df
jfzieg <jzieg@uvm.edu>
2017-11-30 12:04:28 -0500

    Updated organization, two classes now to control car/sensors. Will need to put work into analysis of sensor data and appropriate responses.

--- CarTest.py
+++ CarTest.py
@@ -1,78 +1,86 @@
-# PJ Solomon November 27 2017
-# Joseph Zieg November 29 2017
-# Modified Adafruit Code for RC Car
-# Assumes Steering = Motor1, Drive = Motor4
+"""
+Authors:
+PJ Solomon November 27 2017
+Joseph Zieg November 29 2017
 
-#!/usr/bin/python
+Assumes Steering = Motor1, Drive = Motor4
+
+
+!/usr/bin/python  `
+"""
 
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 from gpiozero import DistanceSensor
-
 import time
 import atexit
 
-# create a default object, no changes to I2C address or frequency
-mh = Adafruit_MotorHAT(addr=0x60)
 
-mySteering = mh.getMotor(1)
-myMotor = mh.getMotor(4)
-
-MAX_SPEED = 255
-
-# recommended for auto-disabling motors on shutdown!
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
-################################# DC motor test!
-
-def test():
-    # set the speed to start, from 0 (off) to 255 (max speed)
-    myMotor.setSpeed(250)
-    myMotor.run(Adafruit_MotorHAT.FORWARD)
-    mySteering.setSpeed(0)
-    mySteering.run(Adafruit_MotorHAT.FORWARD)
-    # turn on motor
-    myMotor.run(Adafruit_MotorHAT.RELEASE)
-    mySteering.run(Adafruit_MotorHAT.RELEASE)
-    atexit.register(turnOffMotors)
-
-def drive():
-    us = sensor(2,3)
-
-    #drives forward while not sensing object
-    while True:
-        myMotor.setSpeed(MAX_SPEED)
-        myMotor.run(Adafruit_MotorHAT.FORWARD)
-
-        #when object is sensed, turn until it is out of range
-        while us.senseObject():
-            #Slow down
-            myMotor.setSpeed(MAX_SPEED / 2)
-
-            #turn for set amount of seconds
-            mySteering.setSpeed(0)
-            mySteering.run(Adafruit_MotorHAT.FORWARD)
-            time.sleep(.01)
-        mySteering.run(Adafruit_MotorHAT.RELEASE)
-
-def main():
-
-    test()
-
-    drive()
-
-    turnOffMotors()
-    atexit.register(turnOffMotors)
-
-class sensor(echo, trigger):
+class car(object):
     """
-    Interfaces with the HR-S04 ultrasonic range sensors to
+    Controls for the rover. Uses input from sensors to
     """
-    ultrasonic = DistanceSensor(echo, trigger, max_distance=1, threshold_distance=.2)
+    def __init__(self, addr=0x60, steering_id=1, drive_id=4):
+        # Initialize HAT and motors
+        self.mh = Adafruit_MotorHAT(addr)
+        self.steering = self.mh.getMotor(steering_id)
+        self.motor = self.mh.getMotor(drive_id)
 
+        self.MAX_SPEED = 255
+
+        # Initialize sensors
+        self.us1 = sensor(2, 3)  # These need to be set to the proper gpio pins
+        self.us2 = sensor(19, 20)
+
+    def turnOffMotors(self):
+        self.steering.run(Adafruit_MotorHAT.RELEASE)
+        self.motor.run(Adafruit_MotorHAT.RELEASE)
+
+    def drive(self):
+        for i in range(100):  # Runs for 10 seconds
+            self.forward(self.MAX_SPEED)
+            if self.us1.senseObject():
+                # Update with turning func
+                self.steering.setSpeed(0)
+                self.steering.run(Adafruit_MotorHAT.FORWARD)
+            time.sleep(.1)
+
+    def forward(self, speed):
+        self.motor.setSpeed(speed)
+        self.motor.run(Adafruit_MotorHAT.FORWARD)
+        time.sleep(.01)
+
+    def backward(self, speed):
+        self.motor.reverse()
+        self.motor.setSpeed(speed)
+        self.motor.run(Adafruit_MotorHAT.BACKWARD)
+
+    def test(self):
+        # set the speed to start, from 0 (off) to 255 (max speed)
+        self.forward(self.MAX_SPEED)
+
+        # Adjust these to use functions eventually
+        self.steering.setSpeed(0)
+        self.steering.run(Adafruit_MotorHAT.FORWARD)
+
+        time.sleep(10)
+
+        # turn off motor
+        self.motor.run(Adafruit_MotorHAT.RELEASE)
+        self.steering.run(Adafruit_MotorHAT.RELEASE)
+
+
+class sensor(object):
+    """
+    Interfaces with the HR-S04 ultrasonic range sensor
+    echo: GPIO pin to ECHO
+    trigger: GPIO pin to TRIGGER
+
+    max_distance: specified max range of the sensor in m
+    threshold_distance: distance at  which when_in_range is triggered
+    """
+
+    def __init__(self, echo, trigger, max_d, trig_d):
+        self.ultrasonic = DistanceSensor(echo, trigger, max_distance=max_d, threshold_distance=trig_d)
 
     def senseObject(self):
         """
@@ -84,43 +92,10 @@ class sensor(echo, trigger):
         else:
             return False
 
-main()
-        # while (True):
-        #     print("\tForward and Right! ")
-        #     myMotor.run(Adafruit_MotorHAT.BACKWARD);
-        #     mySteering.run(Adafruit_MotorHAT.FORWARD)
-        #
-        #     mySteering.setSpeed(250);
-        #
-        #     print("\tSpeed up...")
-        #     for i in range(250):
-        #         myMotor.setSpeed(i)
-        #         time.sleep(0.01)
-        #
-        #     time.sleep(3)
-        #
-        #     print("\tSlow down...")
-        #     for i in reversed(range(250)):
-        #         myMotor.setSpeed(i)
-        #         time.sleep(0.01)
-        #
-        #     print("\tForward and Left!")
-        #     mySteering.run(Adafruit_MotorHAT.BACKWARD)
-        #
-        #     mySteering.setSpeed(250);
-        #
-        #     print("\tSpeed up...")
-        #     for i in range(250):
-        #         myMotor.setSpeed(i)
-        #         time.sleep(0.01)
-        #
-        #     time.sleep(3)
-        #
-        #     print("\tSlow down...")
-        #     for i in reversed(range(250)):
-        #         myMotor.setSpeed(i)
-        #         time.sleep(0.01)
-        #
-        #     print("\tRelease")
-        #     myMotor.run(Adafruit_MotorHAT.RELEASE)
-        #     # time.sleep(1.0)# JEAP-Autonomous-Project
+
+"""
+CODE TO RUN THE CAR. 
+"""
+car = car()
+
+car.test()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JEAP-Autonomous-Project
+    The purpose of this project is to explore simple autonomous systems. The project consists of designing and building
+a motorized rover that is able to move autonomously. The design consists of a remote controlled car coupled with a
+Raspberry Pi 3 Model B+, implementing ultrasonic range detection for obstacle avoidance. We will be overhauling
+the current RC control scheme of the car to allow it to be controlled directly from the Raspberry Pi.
+
+# Obstacle Avoidance
+    Obstacle avoidance will be tested using an array of HR-S04 ultrasonic sensors, with the goal of minimizing error in
+sensor readings that could lead to the rover crashing. The current implemetation uses the DistanceSensor class from
+the `gpiozero` library.
+
+# Rover Control
+    The rover control is tailored to integrate with an RC car powered by 2 DC motors. The motors are driven by an
+Adafruit Stepper Motor HAT and a Raspberry Pi, implemented using the `Adafruit_MotorHAT` library.
+
+###Contributors###
+Everest Witman: Project Manager, Junior Developer
+Joseph Zieg: Senior Developer
+Aaron Seibring: Head Hardware Engineer
+PJ Solomon: Hardware Engineer, Junior Developer
+
