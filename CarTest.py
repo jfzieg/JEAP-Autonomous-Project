bff5922d4cdfa58bae464a60ea65236a7d82a132
jfzieg <jzieg@uvm.edu>
2017-12-04 15:40:06 -0500

    Next attempt fix for sensor input

--- CarTest.py
+++ CarTest.py
@@ -1,42 +1,37 @@
-
-#Authors:
-#PJ Solomon November 27 2017
-#Joseph Zieg November 29 2017
-
-#Assumes Steering = Motor1, Drive = Motor4
-
-
+# Authors:
+# PJ Solomon November 27 2017
+# Joseph Zieg November 29 2017
+#
+# Assumes Steering = Motor1, Drive = Motor4
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-from gpiozero import DistanceSensor
 import RPi.GPIO as GPIO
 import time
-import atexit
 
-GPIO.setmode(GPIO.BCM)
+# Globals for direction
+RIGHT = 1
+LEFT = 2
+STRAIGHT = 3
 
-class car(object):
-# Controls for the rover. Uses input from sensors to determine optimal course
+
+class Car(object):
+    # Controls for the rover. Uses input from sensors to determine optimal course
 
     def __init__(self, addr=0x60, steering_id=1, drive_id=4):
         # Initialize HAT and motors
         self.mh = Adafruit_MotorHAT(addr)
         self.steering = self.mh.getMotor(steering_id)
         self.motor = self.mh.getMotor(drive_id)
-
         self.MAX_SPEED = 255
 
         # Initialize sensors
-        self.us1 = sensor(19, 16, .3) # Makes decision at 30 cm
-        #self.us2 = sensor(19, 20, .3) #Re-enable when more sensors needed
-
-    def turnOffMotors(self):
-        self.steering.run(Adafruit_MotorHAT.RELEASE)
-        self.motor.run(Adafruit_MotorHAT.RELEASE)
+        GPIO.setmode(GPIO.BCM)
+        self.us1 = Sensor(6, 12)  # Defaults to decision made at 30 cm
+        #self.us2 = Sensor(19, 20, .3)  # Re-enable when more sensors needed
 
     def drive(self):
         for i in range(100):  # Runs for 10 seconds
             if self.us1.collisonWarning():
-                self.right(self.MAX_SPEED);
+                self.turn(self.MAX_SPEED, RIGHT)
             else:
                 self.steering.setSpeed(0)
                 self.forward(self.MAX_SPEED)
@@ -52,7 +47,32 @@ class car(object):
         self.motor.setSpeed(speed)
         self.motor.run(Adafruit_MotorHAT.BACKWARD)
 
+    def turn(self, speed, direction):
+        if direction is RIGHT:
+            self.steering.setSpeed(speed)
+            self.motor.setSpeed(speed)
+            self.steering.run(Adafruit_MotorHAT.FORWARD)
+            self.motor.run(Adafruit_MotorHAT.FORWARD)
+        elif direction is LEFT:
+            self.steering.reverse()
+            self.steering.setSpeed(speed)
+            self.motor.setSpeed(speed)
+            self.steering.run(Adafruit_MotorHAT.FORWARD)
+            self.motor.run(Adafruit_MotorHAT.FORWARD)
+
+    def directionDecision(self):
+        # Update once turning is figured out
+        return None
+
+    def turnOff(self):
+        self.steering.run(Adafruit_MotorHAT.RELEASE)
+        self.motor.run(Adafruit_MotorHAT.RELEASE)
+        GPIO.cleanup()
+
     def test(self):
+        # Test sensor output
+        self.us1.distanceTest()
+
         # set the speed to start, from 0 (off) to 255 (max speed)
         self.forward(self.MAX_SPEED)
 
@@ -63,27 +83,19 @@ class car(object):
         time.sleep(10)
 
         # turn off motor
-        self.motor.run(Adafruit_MotorHAT.RELEASE)
-        self.steering.run(Adafruit_MotorHAT.RELEASE)
-
-    def right(self, speed):
-        self.steering.setSpeed(speed);
-        self.motor.setSpeed(speed);
-        self.steering.run(Adafruit_MotorHAT.FORWARD)
-        self.motor.run(Adafruit_MotorHAT.FORWARD)
+        self.turnOff()
 
 
-class sensor(object):
+class Sensor(object):
+    # Sensor object for use with Car class
+    # GPIO code structure for getDistance() function taken from:
+    # https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
 
     def __init__(self, echo, trigger, min_distance=.3):
         self.MIN_DISTANCE = min_distance * 100
         # set GPIO Pins
         self.GPIO_TRIGGER = trigger
         self.GPIO_ECHO = echo
-
-        # set GPIO direction (IN / OUT)
-        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
-        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
 
     def collisonWarning(self):
         if self.getDistance() <= self.MIN_DISTANCE:
@@ -92,47 +104,46 @@ class sensor(object):
 
     def getDistance(self):
         # Returns the distance in cm given by the sensor
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
 
-        # set Trigger to HIGH
+        GPIO.output(self.GPIO_TRIGGER, False)
+        print "Waiting For Sensor To Settle"
+        time.sleep(2)
+
         GPIO.output(self.GPIO_TRIGGER, True)
-
-        # set Trigger after 0.01ms to LOW
         time.sleep(0.00001)
         GPIO.output(self.GPIO_TRIGGER, False)
 
-        startTime = time.time()
-        stopTime = time.time()
-
-        # save StartTime
         while GPIO.input(self.GPIO_ECHO) == 0:
-            startTime = time.time()
+            pulse_start = time.time()
 
-        # save time of arrival
         while GPIO.input(self.GPIO_ECHO) == 1:
-            stopTime = time.time()
+            pulse_end = time.time()
 
-        # time difference between start and arrival
-        TimeElapsed = stopTime - startTime
+        pulse_duration = pulse_end - pulse_start
 
-        # multiply with the sonic speed (34300 cm/s)
-        # and divide by 2, because there and back
-        distance = (TimeElapsed * 34300) / 2
+        distance = pulse_duration * 17150
 
-        return distance
+        distance = round(distance, 2)
+
+        print "Distance:", distance, "cm"
+
+        GPIO.cleanup()
 
     def distanceTest(self):
-        # Tests the sensor input, outputs to console
-        try:
-            while True:
-                dist = self.getDistance()
-                print ("Measured Distance = %.1f cm" % dist)
-                time.sleep(1)
+        # Tests the sensor input, outputs to console fro 10s
+        for i in range(100):
+            dist = self.getDistance()
+            print ("Measured Distance = %.1f cm" % dist)
+            if self.collisonWarning():
+                print "!!Min Distance Reached!!"
+            time.sleep(.1)
 
-                # Reset by pressing CTRL + C
-        except KeyboardInterrupt:
-            print("Measurement stopped by User")
-            GPIO.cleanup()
 
-car = car()
+####################
+# CAR RUNNING CODE #
+####################
+car = Car()
 
 car.test()
